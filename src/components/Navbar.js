074d97c74de2b1e00e0ae017040640bf5b6dc07e
Sandra <alecsandra.serediuc@gmail.com>
2020-10-13 17:29:47 +0200

    Added the navbar component with 2btn

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <button>Planet</button>
+            <button>People</button>
+        </nav>
+     );
+}
+ 
+export default Navbar;
