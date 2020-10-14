3775e7b38603b6213862d26cf29043632d24ac00
Sandra <alecsandra.serediuc@gmail.com>
2020-10-14 09:59:37 +0200

    Instaled query devtools and checked(tested) the config

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -10,7 +10,14 @@ const fetchPlanets = async () =>{
 const Planets = () => {
     // 1st arg- key for the query
     // 2nd arg -function async that is grabbing the data
-    const {data, status}= useQuery('planets', fetchPlanets)
+    const {data, status}= useQuery('planets', fetchPlanets, {
+        //stays fresh longer, before refetch stale data (from 0 to 2000)
+        staleTime: 0,
+        //by default is using cashed data, seting it in the config, it refetching data everytime you change the page
+        cacheTime: 10,
+        //we can use function(as a value) on specific data status fetching, for user notification or other
+        onSuccess: ()=>console.log('data fetched perfectly')
+    })
     console.log(data)
     return (
     <div>
